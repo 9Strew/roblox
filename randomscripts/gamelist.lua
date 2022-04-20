8df1db235ee6d32c5092483187894d381ced2c51
9Strew <83422292+9Strew@users.noreply.github.com>
2022-04-21 00:03:13 +0300

    Create gamelist.lua

---
+++ randomscripts/gamelist.lua
@@ -0,0 +1,12 @@
+local games = {
+    ["9095754532"] = {
+        Name = "Place Name",
+        url = "https://raw.githubusercontent.com/9Strew/roblox/main/randomscripts/test"
+    }
+   ["9414925486"] = {
+        Name = "aeae",
+        url = "https://raw.githubusercontent.com/9Strew/roblox/main/randomscripts/test"
+    }
+}
+
+return games
