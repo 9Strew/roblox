78a54a7f005aca33ea43a0ba7610e819802b169b
9Strew <83422292+9Strew@users.noreply.github.com>
2022-04-21 00:11:18 +0300

    Update gamelist.lua

--- randomscripts/gamelist.lua
+++ randomscripts/gamelist.lua
@@ -2,7 +2,7 @@ getgenv().games = {
     ["9095754532"] = {
         Name = "Place Name",
         url = "https://raw.githubusercontent.com/9Strew/roblox/main/randomscripts/test"
-    }
+    },
    ["9414925486"] = {
         Name = "aeae",
         url = "https://raw.githubusercontent.com/9Strew/roblox/main/randomscripts/test"
